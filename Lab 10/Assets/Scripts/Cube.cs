8373b4d06363b1303488505fd137436b863f2460
Maciej Karpiński <karpfly@gmail.com>
2020-06-09 16:30:17 +0200

    Lab 10

---
+++ Lab 10/Assets/Scripts/Cube.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Cube : MonoBehaviour
+    {
+        private float x = 0;
+        private float y = 0;
+        private float predkoscPoruszania = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            while (predkoscPoruszania == 0)
+            {
+                predkoscPoruszania = Random.Range(-10f, 10f);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Obraca kwadrat wokół własnej osi
+            transform.rotation = Quaternion.Euler(45f, Time.timeSinceLevelLoad * 60f, 45f);
+            x = Input.GetAxis("Horizontal") * Time.deltaTime * predkoscPoruszania;
+            y = Input.GetAxis("Vertical") * Time.deltaTime * predkoscPoruszania;
+            transform.Translate(x, y, 0);
+        }
+    }
+}
---
+++ Lab 10/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameController : MonoBehaviour
+    {
+        public Material[] Materialy;
+        public GameObject Kwadracik;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StworzKwadrat();
+            }
+        }
+
+        void StworzKwadrat()
+        {
+            Vector3 pozycjaVector3 = Input.mousePosition;
+            pozycjaVector3.z = Random.Range(5f, 20f);
+            pozycjaVector3 = Camera.main.ScreenToWorldPoint(pozycjaVector3);
+            Instantiate(Kwadracik, pozycjaVector3, Quaternion.identity);
+            Kwadracik.GetComponent<Renderer>().material = Materialy[Random.Range(0, Materialy.Length)];
+        }
+    }
+}
